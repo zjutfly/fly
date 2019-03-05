6f7891184133b200481debd47063a1739426741f
Congb19 <347006087@qq.com>
2019-03-05 16:23:56 +0800

    3.5 Header

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#kkk").click(function(){
+        $("#flyfly").hide();
+        // document.write(123);
+    });
+});
+
+// $(function () {
+//     var j;
+//     document.write(j);
+//     }
+// );
