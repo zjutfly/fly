3cecb3684a7fae4ce6ae5e09f13bbce665a9a0ef
Congb19 <347006087@qq.com>
2019-03-07 20:13:54 +0800

    3.7 Header

--- js/main.js
+++ js/main.js
@@ -3,10 +3,32 @@ $(document).ready(function(){
         $("#flyfly").hide();
         // document.write(123);
     });
+    //header背景色调变化
+    $(".flyheader").mouseenter(function(){
+        $("#flyheader").css("background","#000000");
+    });
+    $(".flyheader").mouseleave(function(){
+        $("#flyheader").css("background","#1E1D22");
+    });
+    //sidebar
+    var $mainSidebar = $( "#sidebar-main" );
+    $mainSidebar.simplerSidebar( {
+        attr: "sidebar-main",
+        selectors: {
+            trigger: "#sidebar-main-trigger",
+            quitter: ".quitter"
+        },
+        animation: {
+            easing: "easeOutQuint"
+        }
+    } );
+
 });
+
 
 // $(function () {
 //     var j;
 //     document.write(j);
 //     }
 // );
+
