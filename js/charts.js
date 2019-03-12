06dfbc28ef9744a807dd36fb99ad88483e80f0f1
Congb19 <347006087@qq.com>
2019-03-12 15:45:03 +0800

    3.12 init echarts

--- js/charts.js
+++ js/charts.js
@@ -1,25 +1,47 @@
-// 基于准备好的dom，初始化echarts实例
-var myChart = echarts.init(document.getElementById('main'));
+// amount
 
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('amount'));
 // 指定图表的配置项和数据
 var option = {
     title: {
-        text: 'ECharts 入门示例'
+        text: '信件总量趋势图'
     },
     tooltip: {},
     legend: {
-        data:['销量']
+        data:['数量']
     },
     xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        data: ["2018.9","2018.10","2018.11","2018.12","2019.1","2019.2"]
     },
     yAxis: {},
     series: [{
-        name: '销量',
+        name: '数量',
+        type: 'line',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+};
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
+
+// depart
+var departchart = echarts.init(document.getElementById('depart'));
+var option = {
+    title: {
+        text: '信件部门分布图'
+    },
+    tooltip: {},
+    legend: {
+        data:['数量']
+    },
+    xAxis: {
+        data: ["民政局","派出所","雪纺衫","裤子","高跟鞋","袜子"]
+    },
+    yAxis: {},
+    series: [{
+        name: '数量',
         type: 'bar',
         data: [5, 20, 36, 10, 10, 20]
     }]
 };
-
-// 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
+departchart.setOption(option);
--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
+    $("#amount").show();
+    $("#depart").hide();
+    $("#area").hide();
+    $("#type").hide();
+    $("#emotion").hide();
+    $("#hotspot").hide();
+
     $("#kkk").click(function(){
         $("#flyfly").hide();
         // document.write(123);
     });
+
     //header背景色调变化
     $(".flyheader").mouseenter(function(){
         $("#flyheader").css("background","#000000");
@@ -10,13 +18,67 @@ $(document).ready(function(){
     $(".flyheader").mouseleave(function(){
         $("#flyheader").css("background","#1E1D22");
     });
-    //sidebar
+
+    $("#amountbutton").click(function(){
+        $("#amount").show();
+        $("#depart").hide();
+        $("#area").hide();
+        $("#type").hide();
+        $("#emotion").hide();
+        $("#hotspot").hide();
+        // document.write(123);
+    });
+    $("#departbutton").click(function(){
+        $("#amount").hide();
+        $("#depart").show();
+        $("#area").hide();
+        $("#type").hide();
+        $("#emotion").hide();
+        $("#hotspot").hide();
+        // document.write(123);
+    });
+    $("#areabutton").click(function(){
+        $("#amount").hide();
+        $("#depart").hide();
+        $("#area").show();
+        $("#type").hide();
+        $("#emotion").hide();
+        $("#hotspot").hide();
+        // document.write(123);
+    });
+    $("#typebutton").click(function(){
+        $("#amount").hide();
+        $("#depart").hide();
+        $("#area").hide();
+        $("#type").show();
+        $("#emotion").hide();
+        $("#hotspot").hide();
+        // document.write(123);
+    });
+    $("#emotionbutton").click(function(){
+        $("#amount").hide();
+        $("#depart").hide();
+        $("#area").hide();
+        $("#type").hide();
+        $("#emotion").show();
+        $("#hotspot").hide();
+        // document.write(123);
+    });
+    $("#hotspotbutton").click(function(){
+        $("#amount").hide();
+        $("#depart").hide();
+        $("#area").hide();
+        $("#type").hide();
+        $("#emotion").hide();
+        $("#hotspot").show();
+        // document.write(123);
+    });
 
 
 
 });
 
-
+// jquery test
 // $(function () {
 //     var j;
 //     document.write(j);
