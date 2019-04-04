e81ab12a38ce20926ff95d92aca6bb45a9f705ee
Congb19 <347006087@qq.com>
2019-04-04 20:55:01 +0800

    3.31 add mainlist and api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
+var data1;
+function getdata() {
+    $.get("URL",function(datas, error, code){
+        if (code == 200)  alert("数据: " + datas + "\n状态: " + error);
+        else if (code == 201)  alert(error);
+        data1 = datas;
+    })
+}
+//还要修改
+function createtable(data1) {
+    var tableData = "<tr>";
+    for(var i = 0; i < data1.length; i++) {
+        tableData += "<th>" + data1[i].no + "</th>";
+        tableData += "<th>" + data1[i].content + "</th>";
+        tableData += "<th>" + data1[i].time + "</th>";
+    }
+    tableData += "</tr>";
+    $("#tbody1").html(tableData);
+}
+
 $(document).ready(function(){
-    $("#amount").show();
+    //init table
+
+    // getdata();
+    // createtable(data1);
+
+    $("#mainlist").show();
+    $("#amount").hide();
     $("#depart").hide();
     $("#area").hide();
     $("#type").hide();
@@ -19,8 +45,19 @@ $(document).ready(function(){
     // $(".flyheader").mouseleave(function(){
     //     $("#flyheader").css("background","#1E1D22");
     // });
-
+    $("#listbutton").click(function(){
+        $("#mainlist").show();
+        $("#amount").hide();
+        $("#depart").hide();
+        $("#area").hide();
+        $("#type").hide();
+        $("#emotion").hide();
+        $("#hotspot").hide();
+        // document.write(123);
+        $(".page-header").text("投诉线索智能筛查系统");
+    });
     $("#amountbutton").click(function(){
+        $("#mainlist").hide();
         $("#amount").show();
         $("#depart").hide();
         $("#area").hide();
@@ -40,6 +77,7 @@ $(document).ready(function(){
     //     // document.write(123);
     // });
     $("#areabutton").click(function(){
+        $("#mainlist").hide();
         $("#amount").hide();
         $("#depart").hide();
         $("#area").show();
@@ -50,6 +88,7 @@ $(document).ready(function(){
         $(".page-header").text("投诉量按地区分布图");
     });
     $("#typebutton").click(function(){
+        $("#mainlist").hide();
         $("#amount").hide();
         $("#depart").hide();
         $("#area").hide();
@@ -60,6 +99,7 @@ $(document).ready(function(){
         $(".page-header").text("投诉量按地区分布图");
     });
     $("#emotionbutton").click(function(){
+        $("#mainlist").hide();
         $("#amount").hide();
         $("#depart").hide();
         $("#area").hide();
@@ -70,6 +110,7 @@ $(document).ready(function(){
         $(".page-header").text("投诉量按地区分布图");
     });
     $("#hotspotbutton").click(function(){
+        $("#mainlist").hide();
         $("#amount").hide();
         $("#depart").hide();
         $("#area").hide();
@@ -81,6 +122,11 @@ $(document).ready(function(){
     });
 
 
+//refresh list
+    $("#refreshbutton").click(function(){
+        getdata();
+        createtable(data1);
+    });
 
 });
 
@@ -90,4 +136,3 @@ $(document).ready(function(){
 //     document.write(j);
 //     }
 // );
-
